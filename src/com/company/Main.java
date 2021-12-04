3b43e85d82af3730e18c6d96a7ba12f23a8b7303
yperunov <perunovyuri@gmail.com>
2021-12-04 18:57:38 +0300

    Коммит после корректировки. К вс 5.12 еще подобъю второй блок задач

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,11 @@ import java.util.Arrays;
 
 public class Main {
 
+
     public static void main(String[] args) {
 	// write your code here
-        //task1
+        // ARRAYS part1
+        // task1
         int[] m1 = new int[3];
         m1[0] = 1;
         m1[1] = 2;
@@ -18,24 +20,118 @@ public class Main {
 
         char[] m3 = {'Y', 'u', 'r', 'i', 'y'};
 
-        //task2
-        System.out.println(m1[0]+","+m1[1]+","+m1[2]);
-        System.out.println(m2[0]+","+m2[1]+","+m2[2]);
-        System.out.println(m3[0]+","+m3[1]+","+m3[2]+","+m3[3]+","+m3[4]);
+        // System.out.println(Arrays.toString(m1));
 
-        //task3
-        System.out.println(m1[2]+","+m1[1]+","+m1[0]);
-        System.out.println(m2[2]+","+m2[1]+","+m2[0]);
-        System.out.println(m3[4]+","+m3[3]+","+m3[2]+","+m3[1]+","+m3[0]);
+        //task2 (решение AdHoc)
+        System.out.println(m1[0]+", "+m1[1]+", "+m1[2]);
+        System.out.println(m2[0]+", "+m2[1]+", "+m2[2]);
+        System.out.println(m3[0]+", "+m3[1]+", "+m3[2]+", "+m3[3]+", "+m3[4]);
+
+        //task2 (решение с циклами)
+        for (int i = 0; i < m1.length; i++) {
+            if (i != m1.length-1) {
+                System.out.print(m1[i] + ", ");
+            } else {
+                System.out.println(m1[i]);
+            }
+        }
+
+        for (int i = 0; i < m2.length; i++) {
+            if (i != m2.length-1) {
+                System.out.print(m2[i] + ", ");
+            } else {
+                System.out.println(m2[i]);
+            }
+        }
+
+        for (int i = 0; i < m3.length; i++) {
+            if (i != m3.length-1) {
+                System.out.print(m3[i] + ", ");
+            } else {
+                System.out.println(m3[i]);
+            }
+        }
+
+        //task3 (решение AdHoc)
+        System.out.println(m1[2]+", "+m1[1]+", "+m1[0]);
+        System.out.println(m2[2]+", "+m2[1]+", "+m2[0]);
+        System.out.println(m3[4]+", "+m3[3]+", "+m3[2]+", "+m3[1]+", "+m3[0]);
+
+        //task3 (решение с циклами)
+        for (int i = m1.length-1; i >= 0 ; i--) {
+            if (i != 0) {
+                System.out.print(m1[i] + ", ");
+            } else {
+                System.out.println(m1[i]);
+            }
+        }
+
+        for (int i = m2.length-1; i >= 0 ; i--) {
+            if (i != 0) {
+                System.out.print(m2[i] + ", ");
+            } else {
+                System.out.println(m2[i]);
+            }
+        }
+
+        for (int i = m3.length-1; i >= 0 ; i--) {
+            if (i != 0) {
+                System.out.print(m3[i] + ", ");
+            } else {
+                System.out.println(m3[i]);
+            }
+        }
 
         //task4
         for (int i = 0; i < m1.length; i++){
-            if (m1[i] % 2 == 0) {
-                System.out.println(m1[i]);
+            if (m1[i] % 2 != 0) {
+                m1[i] = m1[i] + 1;
             }
-            else {
-                System.out.println(m1[i]+1);
+        }
+        System.out.println(Arrays.toString(m1));
+
+        // ARRAYS part2
+        // Here will be tasks with Arrays part2
+
+        // Saturday Practice with Madjar
+
+        // DOPTASK-1
+
+        int twoD[][] = new int[3][3];
+        for (int i=0; i< twoD.length; i++){
+            for (int j=0; j < twoD[i].length; j++){
+             twoD[i][j]=1;
+             System.out.print(twoD[i][j] + " ");
             }
+            System.out.println();
+        }
+
+        // DOPTASK-2
+
+        int twoC[][] = new int[3][3];
+        int value = 1;
+        for (int i=0; i< twoC.length; i++){
+            for (int j=0; j < twoC[i].length; j++){
+                twoC[i][j]= value;
+                value++;
+                System.out.print(twoC[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        // DOPTASK-3
+        fillArray(4,4);
+
+    }
+    private static void fillArray(int m, int n) {
+        int[][] matrix = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = 1;
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
